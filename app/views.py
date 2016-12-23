adcbc34b71b17fba0ca45ae73d2a9a40d26850c0
Leo Simmons <leooscar.simmons@gmail.com>
2016-12-23 14:13:13 -0500

    handle bad requests

--- app/views.py
+++ app/views.py
@@ -6,12 +6,29 @@ from . import db
 from models import Url
 from controller import encode, decode
 
+err_message = '''
+Please make sure are sending:
+A) a POST request to http://localhost:5000/shorten with form data that has a \'url\' field with your original URL or
+B) a GET request to your generated shortened URL (http://localhost:5000/s/*your_code*).
+'''
+
+conflict_message = 'The shortened URL http://localhost/s/%s has already been created for %s.'
 
 
-@app.route('/shorten', methods=['POST'])
+@app.route('/shorten', methods=['GET', 'HEAD', 'PUT', 'POST', 'DELETE'])
 def shorten():
 
-    true_url = request.form['url']
+    if request.method != 'POST':
+        return err_message, 400
+
+    try:
+        true_url = request.form['url']
+    except:
+        return err_message, 400
+
+    previous_url = Url.query.filter_by(true_url=true_url).first()
+    if previous_url:
+        return conflict_message % (encode(previous_url.id), true_url), 409
 
     url = Url(true_url)
     db.session.add(url)
@@ -20,10 +37,18 @@ def shorten():
     return 'The shortened URL http://localhost:5000/s/%s has been created from your original URL %s.' % (encode(url.id), true_url)
 
 
-@app.route('/s/<path:short>')
+@app.route('/s/<path:short>', methods=['GET', 'HEAD', 'PUT', 'POST', 'DELETE'])
 def reroute(short):
+
+    if request.method != 'GET':
+        return err_message, 400
 
     id = decode(short)
     result = Url.query.filter_by(id=id).first()
 
     return redirect(result.true_url, code=302)
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    return err_message, 404
